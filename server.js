898ac7deba75d6c9a37391391cb6c47e8fabbf8a
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-08-28 23:42:28 -0300

    Routes in server created

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 require('dotenv').config();
-
+const categoryRouter = require('./routes/categories/categoryRoutes');
+const commentRouter = require('./routes/comments/commentRoutes');
+const postRouter = require('./routes/posts/postRoutes');
+const userRouter = require('./routes/users/userRoutes');
 require('./config/dbConnect');
 
 const app = express();
 
 //middlewares
 //routes
+
+//categories route
+app.use('/api/v1/categories', categoryRouter);
+//comments route
+app.use("/api/v1/comments", commentRouter);
+//posts route
+app.use("/api/v1/posts", postRouter);
+//users route
+app.use("/api/v1/users/", userRouter);
+
 //error handler middlewares
 //listen to server
 const PORT = process.env.PORT | 9000;
