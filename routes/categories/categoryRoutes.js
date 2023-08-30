e5f29085bb206bf089f2424cfa8cf7547f493776
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-08-29 23:14:25 -0300

    Category and comment controller created

--- routes/categories/categoryRoutes.js
+++ routes/categories/categoryRoutes.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const {
-  categoryDetailsCtrl,
-  createCategoryCtrl,
-  deleteCategoryCtrl,
-  updateCategoryCtrl,
-  fetchCategoriesCtrl,
-} = require("../../controllers/categories/categoriesCtrl");
+  categoryDetails,
+  createCategory,
+  deleteCategory,
+  updateCategory,
+  fetchCategories,
+} = require("../../controllers/categories/categoriesController");
 const isLogin = require("../../middlewares/isLogin");
 
 const categoryRouter = express.Router();
 
 //POST/api/v1/categories
-categoryRouter.post("/", isLogin, createCategoryCtrl);
+categoryRouter.post("/", isLogin, createCategory);
 
 //GET/api/v1/categories
-categoryRouter.get("/", fetchCategoriesCtrl);
+categoryRouter.get("/", fetchCategories);
 //GET/api/v1/categories/:id
-categoryRouter.get("/:id", categoryDetailsCtrl);
+categoryRouter.get("/:id", categoryDetails);
 
 //DELETE/api/v1/categories/:id
-categoryRouter.delete("/:id", isLogin, deleteCategoryCtrl);
+categoryRouter.delete("/:id", isLogin, deleteCategory);
 
 //PUT/api/v1/categories/:id
-categoryRouter.put("/:id", isLogin, updateCategoryCtrl);
+categoryRouter.put("/:id", isLogin, updateCategory);
 
 module.exports = categoryRouter;
